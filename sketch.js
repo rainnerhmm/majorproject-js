d957b6d65d57780c874807826efeedfc9d6b7fd7
rainner <rainner_hmm@outlook.com>
2024-11-18 14:09:56 -0600

    added majorproject template

---
+++ sketch.js
@@ -0,0 +1,17 @@
+// majorproject
+// Rainn Morphy
+// Nov 18th, 2024
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(220);
+
+  circle(mouseX, mouseY, 100);
+}
