9238e7a7f4ffd272403805e78d0e79a382e9c1ef
rainner <rainner_hmm@outlook.com>
2024-11-22 14:16:46 -0600

    added inspirational links

--- sketch.js
+++ sketch.js
@@ -5,16 +5,21 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+// links 
+// https://p5js.org/examples/
+
+
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(windowWidth, windowHeight, WEBGL);
 }
 
 function draw() {
   background(220);
+  orbitControl();
 
   push();
-  translate(mouseX, mouseY, 0);
+  translate(mouseX-800, mouseY-400, 0);
   sphere(50);
   pop();
 }
